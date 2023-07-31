330a966e25e910788cde1e851a288c12abbb092c
aniket mishra <aniketmishra300@gmail.com>
2023-07-31 22:52:02 +0530

    Array of objects

---
+++ script.js
@@ -0,0 +1,121 @@
+
+// Ouestion Number. 1
+
+// let studentRecords = [ {name: 'John', id: 123, marks : 98 },
+//           {name: 'Baba', id: 101, marks : 23 },
+//           {name: 'yaga', id: 200, marks : 45 },
+//           {name: 'Wick', id: 115, marks : 75 } ] 
+
+// let ans = studentRecords.map((element)=>element.name.toUpperCase() 
+// )
+//     console.log(ans);
+
+
+// Question Number 2
+
+// let studentRecords = [ {name: 'John', id: 123, marks : 98 },
+//           {name: 'Baba', id: 101, marks : 23 },
+//           {name: 'yaga', id: 200, marks : 45 },
+//           {name: 'Wick', id: 115, marks : 75 } ] 
+
+// let marks = studentRecords.filter((element)=>element.marks>50 )
+//     console.log(marks);
+
+
+// Question Number 3
+
+
+// let studentRecords = [ {name: 'John', id: 123, marks : 98 },
+//           {name: 'Baba', id: 101, marks : 23 },
+//           {name: 'yaga', id: 200, marks : 45 },
+//           {name: 'Wick', id: 115, marks : 75 } ] 
+
+// let marks = studentRecords.filter((element)=>element.marks>50 && element.id>120 )
+//     console.log(marks);
+
+
+
+// Question Number 4 ********
+
+// let studentRecords = [ {name: 'John', id: 123, marks : 98 },
+//           {name: 'Baba', id: 101, marks : 23 },
+//           {name: 'yaga', id: 200, marks : 45 },
+//           {name: 'Wick', id: 115, marks : 75 } ] 
+
+// let sum =0;
+// let marks = studentRecords.map((element)=>{
+//      sum += element.marks;
+// })
+// console.log(marks);
+
+
+
+// Question Number 5 
+
+
+// let studentRecords = [ {name: 'John', id: 123, marks : 98 },
+//           {name: 'Baba', id: 101, marks : 23 },
+//           {name: 'yaga', id: 200, marks : 45 },
+//           {name: 'Wick', id: 115, marks : 75 } ] 
+
+// let marks = studentRecords.filter((element)=>element.marks>50)
+// .map((element)=>console.log(element.name))
+
+
+
+// Question Number 7
+
+
+// let studentRecords = [ {name: 'John', id: 123, marks : 98 },
+//           {name: 'Baba', id: 101, marks : 23 },
+//           {name: 'yaga', id: 200, marks : 45 },
+//           {name: 'Wick', id: 115, marks : 75 } ] 
+// let ans =0;
+// let marks = studentRecords.filter((element)=>element.marks<50)
+// .map((element)=>(element.marks+15))
+// .map((element)=>(element.marks>50 ? ans+=element.marks:null))
+// console.log(marks)
+
+
+
+//////////  Question Number 8
+
+// let obj = {
+//     name : "A",
+//     class : "BCA-3",
+//     rollno: 12
+// }
+
+// let obj1 = {
+//     name : "B",
+//     class : "BCA-3",
+//     rollno: 13
+// }
+
+// let obj2 = {
+//     name : "C",
+//     class : "BCA-3",
+//     rollno: 14
+// }
+
+// let obj3 = {
+//     name : "D",
+//     class : "BCA-3",
+//     rollno: 15
+// }
+
+// let obj4 = {
+//     name : "E",
+//     class : "BCA-3",
+//     rollno: 16
+// }
+
+// let obj5 = {
+//     name : "F",
+//     class : "BCA-3",
+//     rollno: 17
+// }
+
+
+// let arr = [obj ,obj1, obj2, obj3 , obj4 , obj5]
+// console.log(arr)
